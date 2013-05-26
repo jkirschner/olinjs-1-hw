c4a2fd20e54dbcf65191773bfbb09df56e0a10ae
Jared A. Kirschner <jared@Jared-Latitude-E6400.(none)>
2013-05-25 17:00:18 -0700

    Added request handler.

--- router.js
+++ router.js
@@ -1,7 +1,13 @@
 var querystring = require("querystring");
 
-function route(pathname) {
+function route(handle, pathname) {
 	console.log("Routing request for " + pathname + ".");
+	if (typeof handle[pathname] === 'function') {
+		handle[pathname]();
+	} else {
+		console.log("No request handler found for " + pathname + ".");
+	}
+	handle[pathname]
 }
 
 exports.route = route;
