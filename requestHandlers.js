6c9d81764895b655b775a589fcc8df0be17a13df
Jared A. Kirschner <jared@Jared-Latitude-E6400.(none)>
2013-05-26 13:43:41 -0700

    Response passed through request handler.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,15 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
 	console.log("Request handler 'start' was called.");
+	response.write("Hello Start");
+	response.end();
 }
 
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called.");
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -1,13 +1,16 @@
 var querystring = require("querystring");
 
-function route(handle, pathname) {
+function route(handle, pathname, response) {
 	console.log("Routing request for " + pathname + ".");
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname]();
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		handle[pathname](response);
 	} else {
 		console.log("No request handler found for " + pathname + ".");
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 not found");
+		response.end()
 	}
-	handle[pathname]
 }
 
 exports.route = route;
