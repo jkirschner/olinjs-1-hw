5c7109317614134b5dbc179dad30d3caf19b8aee
Jared A. Kirschner <jared@jaredkirschner.com>
2013-05-27 12:36:50 -0700

    Image file upload and display added.

--- router.js
+++ router.js
@@ -1,14 +1,12 @@
-var querystring = require("querystring");
-
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
 	console.log("Routing request for " + pathname + ".");
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname](response, postData);
+		handle[pathname](response, request);
 	} else {
 		console.log("No request handler found for " + pathname + ".");
 		response.writeHead(404, {"Content-Type": "text/plain"});
 		response.write("404 not found");
-		response.end()
+		response.end();
 	}
 }
 
