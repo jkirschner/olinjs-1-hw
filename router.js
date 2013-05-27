9274c3715ce5e90abc773298d05de66452798c0b
Jared A. Kirschner <jared@jaredkirschner.com>
2013-05-26 19:45:14 -0700

    Added ability to receive text from POST on start page.

--- router.js
+++ router.js
@@ -1,10 +1,9 @@
 var querystring = require("querystring");
 
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
 	console.log("Routing request for " + pathname + ".");
 	if (typeof handle[pathname] === 'function') {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		handle[pathname](response);
+		handle[pathname](response, postData);
 	} else {
 		console.log("No request handler found for " + pathname + ".");
 		response.writeHead(404, {"Content-Type": "text/plain"});
